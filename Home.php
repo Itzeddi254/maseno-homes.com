4970185fe6d95b756b8173ebcb7c5d53afbb3571
Itzeddi254 <otienoedmond16@gmail.com>
2025-03-03 17:27:52 +0300

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MASENO HOMES</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <section class="header">
+        <nav>
+            <div class="nav-links">
+                <ul>
+                </ul>
+
+            </div>
+        </nav>
+<div class="text-box">
+    <h1>The best home booking <br>&<br>Hostel Management Services</h1>
+    <p>booking rooms made simple using this simple app called Cozy Bookings<br> visit us and get a home </p>
+    <a href="index.html" class="hero-btn">visit us to learn more </a>
+
+</div>        
+
+    </section>
+    
+</body>
+</html>
